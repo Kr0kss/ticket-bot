70827f27233272806e6ff110c122d08a87e89588
ForGetFulSkyBro <47897305+forgetfulskybro@users.noreply.github.com>
2022-04-12 20:06:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {
     MessageButton
 } = require('discord.js');
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.DIRECT_MESSAGES],
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
     partials: ["MESSAGE", "USER"]
 });
 const enmap = require('enmap');
@@ -14,6 +14,7 @@ const {
     token,
     prefix
 } = require('./config.json');
+
 
 const settings = new enmap({
     name: "settings",
